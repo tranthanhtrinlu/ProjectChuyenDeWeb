d5ba97a2bcc565aebd9674b8fcff7fea109ed28f
tranthanhtrinonglam <21130578@st.hcmuaf.edu.vn>
2025-05-29 13:12:19 +0700

    upload code front end

---
+++ react-vite-jobhunter-chuyendeweb/src/components/client/footer.client.tsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <footer style={{ padding: 15, textAlign: "center" }}>
+            {/* <div>Frontend React Typescript - Series Nest.JS Basic &copy; Thanh Tri</div> */}
+        </footer>
+    )
+}
+
+export default Footer;
